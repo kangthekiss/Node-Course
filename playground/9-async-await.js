081548c11cc1e3a18cf0a5df304aabcfb6406313
warapolj-dev <warapolj.dev@gmail.com>
2020-04-28 00:10:41 +0700

    test handler async-await

--- playground/9-async-await.js
+++ playground/9-async-await.js
@@ -1,23 +1,33 @@
 const add = (a, b) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if(a < 0 || b < 0) 
-                return reject('Numbers must bo non-negative')
+            if (a < 0 || b < 0) return reject('Numbers must bo non-negative');
 
-            resolve(a + b)
-        }, 2000)
-    })
-}
+            resolve(a + b);
+        }, 2000);
+    });
+};
 
 const doWork = async () => {
-    const sum = await add(1, 99)
-    const sum2 = await add(sum, 50)
-    const sum3 = await add(sum2, -1)
-    return sum3
-}
+    const sum = await add(1, 99);
+    const sum2 = await add(sum, 50);
+    const sum3 = await add(sum2, 1);
+    return sum3;
+};
 
-doWork().then((result) => {
-    console.log('result ', result)
-}).catch((err) => {
-    console.log(err)
-})
+const doWork2 = async () => {
+    const sum = await add(1, 99);
+    return sum;
+};
+
+doWork()
+    .then((result) => {
+        console.log('result ', result);
+        return doWork2();
+    })
+    .then((result2) => {
+        console.log('result2 ', result2);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
